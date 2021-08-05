2231522181302f7e165324e9be18d24ddab1c525
Birkyboy <birkyboy@icloud.com>
2021-08-05 18:50:52 +0200

    Initial Commit

---
+++ Le BaluchonTests/Le_BaluchonTests.swift
@@ -0,0 +1,33 @@
+//
+//  Le_BaluchonTests.swift
+//  Le BaluchonTests
+//
+//  Created by Birkyboy on 05/08/2021.
+//
+
+import XCTest
+@testable import Le_Baluchon
+
+class Le_BaluchonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
