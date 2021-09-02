28e0125aa3dbcc65c1f0468ec60519e6818fe0e1
birkyboy <49998863+birkyboydesign@users.noreply.github.com>
2021-09-02 08:13:01 +0200

    Update ApiKeys.swift

--- Le Baluchon/Helpers/ApiKeys.swift
+++ Le Baluchon/Helpers/ApiKeys.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct ApiKeys {
-    static let ifixerKEY = "9d302d668b532babe3dcf93c5e973670"
-    static let googleTranslateKey = "AIzaSyCEbVNspE4p1PakwXtzEG-ifo2wy4_mVZQ"
-    static let openWeatherKEY = "4cf19faf1d4a44b516668cf796eaa3a0"
+    static let ifixerKEY = ""
+    static let googleTranslateKey = ""
+    static let openWeatherKEY = ""
 }
